80573b458edc363fb397b7d4b0f5cd7d47fcc525
florianthalmann <florianthalmann@4d33d0ce-c129-4e39-8b9b-3515391b8597>
2013-07-30 16:57:30 +0000

    graph visualization added! selection and editing of transformations works!! pure awesomeness

--- java/src/org/rubato/rubettes/bigbang/model/BigBangTransformationGraph.java
+++ java/src/org/rubato/rubettes/bigbang/model/BigBangTransformationGraph.java
@@ -3,18 +3,19 @@ package org.rubato.rubettes.bigbang.model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.rubato.rubettes.bigbang.model.edits.AbstractOperationEdit;
 import org.rubato.rubettes.bigbang.model.edits.AbstractTransformationEdit;
 import org.rubato.rubettes.util.DenotatorPath;
 
-public class BigBangTransformationGraph extends ArrayList<AbstractOperationEdit> {
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+
+public class BigBangTransformationGraph extends DirectedSparseGraph<Integer,AbstractOperationEdit> {
 	
 	public BigBangTransformationGraph() {
+		this.addVertex(0);
 	}
 	
-	@Override
 	public boolean add(AbstractOperationEdit edit) {
 		return this.add(edit, false);
 	}
@@ -25,7 +26,8 @@ public class BigBangTransformationGraph extends ArrayList<AbstractOperationEdit>
 	}
 	
 	private boolean add(AbstractOperationEdit edit, boolean inPreviewMode) {
-		boolean added = super.add(edit);
+		this.addVertex(this.getVertexCount());
+		boolean added = this.addEdge(edit, this.getVertexCount()-2, this.getVertexCount()-1);
 		if (added) {
 			this.updateScore(inPreviewMode);
 		}
@@ -33,14 +35,17 @@ public class BigBangTransformationGraph extends ArrayList<AbstractOperationEdit>
 	}
 	
 	public void updateScore(boolean inPreviewMode) {
-		if (this.size()>0) {
+		if (this.getEdgeCount() > 0) {
 			List<Map<DenotatorPath,DenotatorPath>> pathDifferences = new ArrayList<Map<DenotatorPath,DenotatorPath>>();
-			this.get(this.size()-1).setInPreviewMode(inPreviewMode);
-			this.get(0).getScoreManager().resetScore();
-			for (int i = 0; i < this.size(); i++) {
-				AbstractOperationEdit edit = this.get(i);
+			//TODO for now just sets inPreviewMode for the last edge.
+			AbstractOperationEdit lastEdge = this.getInEdges(this.getEdgeCount()).iterator().next(); 
+			lastEdge.setInPreviewMode(inPreviewMode);
+			//TODO pretty bad...
+			lastEdge.getScoreManager().resetScore();
+			for (int i = 0; i < this.getEdgeCount(); i++) {
+				AbstractOperationEdit currentEdit = this.findEdge(i, i+1);
 				//only send composition change with last one!!!!!!
-				pathDifferences = edit.execute(pathDifferences, i==this.size()-1);
+				pathDifferences = currentEdit.execute(pathDifferences, i==this.getEdgeCount()-1);
 			}
 		}
 	}
@@ -52,7 +57,15 @@ public class BigBangTransformationGraph extends ArrayList<AbstractOperationEdit>
 	}
 	
 	private AbstractOperationEdit removeLastWithoutUpdate() {
-		return super.remove(this.size()-1);
+		AbstractOperationEdit lastEdit = this.getLastEdit();
+		this.removeEdge(lastEdit);
+		this.removeVertex(this.getVertexCount()-1);
+		return lastEdit;
+	}
+	
+	public AbstractOperationEdit getLastEdit() {
+		int lastVertex = this.getEdgeCount();
+		return this.findEdge(lastVertex-1, lastVertex);
 	}
 
 }
